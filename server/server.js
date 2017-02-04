0c04927849a6a4656fc08159205e3220be97f547
milosacimovic <milosacimovic@hotmail.com>
2017-02-05 00:43:25 +0100

    Established socket.io in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '/../public')
-
 const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
+/*configure middleware*/
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected.');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
